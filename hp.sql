5dfeb3c057a804d82d2daa7e00b54d2141c105d9
Alex <janouser11@gmail.com>
2016-11-08 14:52:17 -0500

    add

--- hp.sql
+++ hp.sql
@@ -1,4 +1,5 @@
-#Harry Potter Database SQL Script
+-- Harry Potter Database SQL Script
+-- @Authors: Charles Harrill, Greg Eskridge and Alex Janousek
 
 
 
@@ -29,4 +30,37 @@ CREATE TABLE HP_LANGUAGE
 Language_Name  VARCHAR(50) NOT NULL,
 Character_Name VARCHAR(50) NOT NULL,
 Constraint HP_LANGUAGE_PK Primary Key (Language_Name, Character_Name)
+);
+
+CREATE TABLE HP_GROUP
+(
+Group_Name VARCHAR(50),
+Purpose VARCHAR(50),
+Ethical_Orientation VARCHAR(50),
+Wizard_Name VARCHAR(50),
+Constraint HP_GROUP Primary Key (Group_Name)
+
+);
+
+CREATE TABLE HP_GROUP_WIZARD_AFFILIATION
+(
+Group_Name VARCHAR(50),
+Wizard_Name VARCHAR(50),
+Constraint HP_GROUP_WIZARD_AFFILIATION Primary Key (Group_Name, Wizard_Name)
+);
+
+CREATE TABLE HP_POTION_RECIPE_ID
+(
+  -- Do we want string or numerical ID?
+Potion_Recipe_Id VARCHAR(),
+Potion_Name VARCHAR(50),
+Ingredient_Name VARCHAR(50),
+Constraint HP_POTION_RECIPE_ID Primary Key (Potion_Recipe_Id, Potion_Name, Ingredient_Name)
+);
+
+CREATE TABLE HP_POTION_INGREDIENT
+(
+Potion_Ingredient_Name VARCHAR(50),
+Location_Found VARCHAR(50),
+Constraint HP_POTION_INGREDIENT Primary Key (Potion_Ingredient_Name)
 );
