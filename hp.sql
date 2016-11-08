77dc06d4b40ea52f43334a21a91959929a8c8e87
Alex <janouser11@gmail.com>
2016-11-08 13:59:39 -0500

    add

--- hp.sql
+++ hp.sql
@@ -13,20 +13,20 @@ Constraint HP_SCHOOL_PK Primary Key (School_Name)
 
 CREATE TABLE HP_CHARACTER
 (
-Character_Name VARCHAR(50),
-Level_Of_Impact VARCHAR(50),
+Character_Name      VARCHAR(50),
+Level_Of_Impact     VARCHAR(50),
   Check(Level_Of_Impact IN('All Books', 'MostBooks', 'Less Than Half of Books', 'One Chapter', 'Referenced')),
 Human CHAR(1),
 Creature CHAR(1),
 School_Name_Visited VARCHAR(50),
-House_Name_Belongs VARCHAR(50),
+House_Name_Belongs  VARCHAR(50),
 Constraint HP_CHARACTER_PK Primary Key (Character_Name)
 );
 
 
 CREATE TABLE HP_LANGUAGE
 (
-Language_Name VARCHAR(50) NOT NULL,
+Language_Name  VARCHAR(50) NOT NULL,
 Character_Name VARCHAR(50) NOT NULL,
 Constraint HP_LANGUAGE_PK Primary Key (Language_Name, Character_Name)
 );
