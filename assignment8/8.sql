20a8e792742b91012bb99196dc62921b2fa9b932
Alex <janouser11@gmail.com>
2016-11-22 10:22:34 -0500

    Completed

--- assignment8/8.sql
+++ assignment8/8.sql
@@ -1,18 +1,18 @@
--- Question 1    
+-- Question 1
 Select Customer_Id, Customer_Name
     From S_Customer_T;
 
 -- Question 2
 Select Customer_Name AS "Name", Customer_Address AS "Address"
     From S_Customer_T
-    Where Customer_Name = 'Home Furnishings';
+      Where Customer_Name = 'Home Furnishings';
 
--- Question 3    
+-- Question 3
 Select Product_ID
     From S_Product_T
-    Where Standard_Price < 200;
+      Where Standard_Price < 200;
 
--- Question 4    
+-- Question 4
 Select AVG(Standard_Price)
     From S_Product_T;
 
@@ -27,14 +27,14 @@ Select COUNT(Order_ID)
 -- Question 7
 Select COUNT(Product_ID)
     From S_Order_Line_T
-    Where Order_ID = 1006;
+      Where Order_ID = 1006;
 
 -- Question 8
 Select *
     From S_Order_T
-    Where Order_Date < '10/24/0008';
+      Where Order_Date < '10/24/0008';
 
 -- Question 9
 Select MAX(Product_Description)
     As "Name"
-    From S_Product_T;
+      From S_Product_T;
--- assignment9/9.sql
+++ assignment9/9.sql
@@ -1,46 +1,46 @@
 -- Question 1
-select Customer_Name
-from S_Customer_t,S_ORDER_t
-where s_customer_t.customer_id = S_ORDER_t.customer_id
+Select Customer_Name
+  from S_Customer_t,S_ORDER_t
+    where s_customer_t.customer_id = S_ORDER_t.customer_id
 
 -- Question 2
-select product_description
-from s_product_t, s_order_line_t, s_order_t
-where s_product_t.product_id = s_order_line_t.product_id
-and s_order_line_t.order_id = s_order_t.order_id
-and customer_id = 1;
+Select product_description
+  from s_product_t, s_order_line_t, s_order_t
+    where s_product_t.product_id = s_order_line_t.product_id
+      and s_order_line_t.order_id = s_order_t.order_id
+      and customer_id = 1;
 
 -- Question 3
-select product_description
-from s_product_t, s_order_line_t, s_order_t, s_customer_t
-where s_product_t.product_id = s_order_line_t.product_id
-and s_order_line_t.order_id = s_order_t.order_id
-and s_order_t.customer_id = s_customer_t.customer_id
-and customer_name = 'Impressions';
+Select product_description
+  from s_product_t, s_order_line_t, s_order_t, s_customer_t
+    where s_product_t.product_id = s_order_line_t.product_id
+      and s_order_line_t.order_id = s_order_t.order_id
+      and s_order_t.customer_id = s_customer_t.customer_id
+      and customer_name = 'Impressions';
 
 -- Question 4
-select customer_name
-from s_customer_t, s_order_t, s_order_line_t, s_product_t
-where s_customer_t.customer_id = s_order_t.customer_id
-and s_order_t.order_id = s_order_line_t.order_id
-and s_order_line_t.product_id = s_product_t.product_id
-and standard_price > 500;
+Select customer_name
+  from s_customer_t, s_order_t, s_order_line_t, s_product_t
+    where s_customer_t.customer_id = s_order_t.customer_id
+      and s_order_t.order_id = s_order_line_t.order_id
+      and s_order_line_t.product_id = s_product_t.product_id
+      and standard_price > 500;
 
 -- Question 5
-select s_customer_t.*, s_order_t.order_id
-from s_customer_t
-left outer join s_order_t
-on s_customer_t.customer_id=s_order_t.customer_id;
+Select s_customer_t.*, s_order_t.order_id
+  from s_customer_t
+  left outer join s_order_t
+  on s_customer_t.customer_id=s_order_t.customer_id;
 
 -- Question 6
 SELECT ORDER_ID, S_PRODUCT_T.PRODUCT_ID, STANDARD_PRICE, ORDERED_QUANTITY, STANDARD_PRICE * ORDERED_QUANTITY
-FROM S_PRODUCT_T, S_ORDER_LINE_T
-WHERE S_PRODUCT_T.PRODUCT_ID = S_ORDER_LINE_T.PRODUCT_ID
-ORDER BY ORDER_ID, S_PRODUCT_T.PRODUCT_ID;
+  FROM S_PRODUCT_T, S_ORDER_LINE_T
+    WHERE S_PRODUCT_T.PRODUCT_ID = S_ORDER_LINE_T.PRODUCT_ID
+    ORDER BY ORDER_ID, S_PRODUCT_T.PRODUCT_ID;
 
 -- Question 7
 SELECT ORDER_ID, SUM(STANDARD_PRICE * ORDERED_QUANTITY)
-FROM S_PRODUCT_T, S_ORDER_LINE_T
-WHERE S_PRODUCT_T.PRODUCT_ID = S_ORDER_LINE_T.PRODUCT_ID
-GROUP BY ORDER_ID
-ORDER BY ORDER_ID;
+  FROM S_PRODUCT_T, S_ORDER_LINE_T
+    WHERE S_PRODUCT_T.PRODUCT_ID = S_ORDER_LINE_T.PRODUCT_ID
+      GROUP BY ORDER_ID
+        ORDER BY ORDER_ID;
